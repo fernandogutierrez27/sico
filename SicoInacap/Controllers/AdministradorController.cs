6d556ec1aa66b112df8de5f512b4671544b55eb3
Fernando <fernando.gutierrez@live.cl>
2018-07-03 01:32:50 -0400

    RSW101 Listo, RSW02 Listo, RSW103 Listo, RSW202 Listo

---
+++ SicoInacap/Controllers/AdministradorController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SicoInacap.Models;
+
+namespace SicoInacap.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        private SicoModel db = new SicoModel();
+
+        // GET: Administrador
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Busca el administrador
+        public Administrador GetAdministrador(Usuario usuario)
+        {
+            bool encontrado = false;
+            Administrador administrador = new Administrador();
+
+            foreach(Administrador admin in db.Administrador.ToList())
+            { 
+                if (admin.Username.ToUpper().Equals(usuario.Username.ToUpper()))
+                {
+                    administrador = admin;
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            if (encontrado)
+            {
+                return administrador;
+            }
+            else
+            {
+                return null;
+
+            }
+            
+        }
+    }
+}
---
+++ SicoInacap/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SicoInacap.Models;
+
+namespace SicoInacap.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private SicoModel db = new SicoModel();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Realiza el Login
+        [HttpPost]
+        public ActionResult Authorize(SicoInacap.Models.Usuario usuarioLogin)
+        {
+
+            Usuario usuario = db.Usuario.Where(u => u.Username.ToUpper() == usuarioLogin.Username.ToUpper() && u.Password.ToUpper() == usuarioLogin.Password.ToUpper()).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                ViewData["error"] = "Username o Password incorrecto.";
+                //return View("index", usuarioLogin);
+                return View("index");
+            }
+            else
+            {
+
+                SimpatizanteController simpController = new SimpatizanteController();
+
+                usuario.Simpatizante = simpController.GetSimpatizante(usuario);
+
+                AdministradorController adminController = new AdministradorController();
+
+                usuario.Administrador = adminController.GetAdministrador(usuario);
+
+
+                if (usuario.Simpatizante == null || usuario.Administrador == null)
+                {
+                    ViewData["error"] = "Usted debe ser Simpatizante y Administrador para ingresar.";
+                    return View("index");
+                }
+                else
+                {
+                        Session["nombreUsuario"] = usuario.Simpatizante.Nombres + " " + usuario.Simpatizante.Apellidos;
+                        Session["username"] = usuario.Username;
+                    
+                    return RedirectToAction("index", "Home");
+                }
+                
+            }
+        }
+
+        //Cierra Sesión
+        public ActionResult Logout()
+        {
+                Session.Abandon();
+                return RedirectToAction("index", "Login");
+        }
+    }
+}
